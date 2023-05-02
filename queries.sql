ac2fc5240d710dd4ec874df9c3db437b53e94a91
cabraham1 <christopherabraham8@gmail.com>
2023-05-02 17:55:30 +0100

    udate quaries

--- queries.sql
+++ queries.sql
@@ -97,3 +97,25 @@ UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 
 -- Commit the transaction
 COMMIT;
+
+
+SELECT COUNT(*) AS total_animals FROM animals;
+
+SELECT COUNT(*) AS non_escapers FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) AS avg_weight FROM animals;
+
+SELECT neutered, SUM(escape_attempts) AS total_escapes
+FROM animals
+GROUP BY neutered
+ORDER BY total_escapes DESC
+LIMIT 1;
+
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) AS avg_escapes
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
