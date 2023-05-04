2f388cce9f3bdf3937a0c8e3e61fc6fb437d2644
cabraham1 <christopherabraham8@gmail.com>
2023-05-04 21:37:03 +0100

    Update schema

--- schema.sql
+++ schema.sql
@@ -98,3 +98,34 @@ ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 -- changed visit_date TO date_of_visit
 ALTER TABLE visits RENAME COLUMN visit_date TO date_of_visit;
 
+
+-- Create a table named vets
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  age INTEGER,
+  date_of_graduation DATE
+);
+
+
+-- Create a "join table" called specializations
+CREATE TABLE specializations (
+  vet_id INTEGER REFERENCES vets (id),
+  species_id INTEGER REFERENCES species (id),
+  PRIMARY KEY (vet_id, species_id)
+);
+
+
+-- Create a "join table" called visits
+CREATE TABLE visits (
+  id SERIAL PRIMARY KEY,
+  animal_id INTEGER REFERENCES animals(id),
+  vet_id INTEGER REFERENCES vets(id),
+  visit_date DATE
+);
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+-- changed visit_date TO date_of_visit
+ALTER TABLE visits RENAME COLUMN visit_date TO date_of_visit;
